10f55617e5570234613fb722f55b3b8463f27492
Qmanh <quanmanh901@gmail.com>
2024-07-17 23:26:06 +0700

    forgot password screen

---
+++ app/src/main/java/com/example/appshoestore/Screen/ForgotPasswordScreen.kt
@@ -0,0 +1,104 @@
+package com.example.appshoestore.Screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIos
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.appshoestore.Constant.CustomOutlinedTextField
+import com.example.appshoestore.R
+
+@Preview
+@Composable
+fun ForgotPasswordScreen() {
+    var scrollState = rememberScrollState()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(start = 22.dp, end = 22.dp)
+            .verticalScroll(scrollState)
+    ) {
+        Column() {
+            Spacer(modifier = Modifier.height(16.dp))
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBackIos,
+                    contentDescription = null,
+                    tint = Color.Black
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Forgot Password",
+                style = TextStyle(
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            )
+            Spacer(modifier = Modifier.height(6.dp))
+            Text(
+                text = "select which contact detail should we use to reset your password.",
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.Gray
+                )
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            CustomOutlinedTextField(
+                title = "Email Address",
+                value = "quanmanh@Gmail.com",
+                modifier = Modifier.fillMaxWidth()
+            )
+            Image(
+                painter = painterResource(id = R.drawable.img_forgot), contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(450.dp)
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color(0xFFFFA500)
+                )
+            ) {
+                Text(
+                    text = "Continue",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                )
+            }
+        }
+    }
+}
