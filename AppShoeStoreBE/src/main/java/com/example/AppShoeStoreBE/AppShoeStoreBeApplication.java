d15829bf2e970a3f86db93c7c9cd9091373665d4
Qmanh <quanmanh901@gmail.com>
2024-07-30 22:42:55 +0700

    create backend

---
+++ AppShoeStoreBE/src/main/java/com/example/AppShoeStoreBE/AppShoeStoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.AppShoeStoreBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppShoeStoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppShoeStoreBeApplication.class, args);
+	}
+
+}
---
+++ AppShoeStoreBE/src/test/java/com/example/AppShoeStoreBE/AppShoeStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppShoeStoreBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppShoeStoreBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
